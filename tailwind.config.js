eb6619f3edf08387695097e629ac4b6b05ededf0
Pol Stafford <redaikidoka@gmail.com>
2021-05-10 15:34:11 -0700

    getting going - copy tailwind template

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+module.exports = {
+  important: true,
+  mode: 'jit',
+  purge: {
+    enabled: process.env.NODE_ENV === "production",
+    content: ["./src/**/*.{html,ts}"],
+  },
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
