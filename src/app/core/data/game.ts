c394fbf418b920515ee478ae1f98f1b208e19c5a
Pol Stafford <gandalf@Mithrandir.local>
2021-06-07 16:38:24 -0700

    pushing out game object setups

--- src/app/core/data/game.ts
+++ src/app/core/data/game.ts
@@ -22,4 +22,80 @@ export interface Game {
 
   idSystem?: number;
   systemName?: string;
+
+  currentSessionId?: number;
+  currentSession?: GameSession;
+  // added bits
+  areas?: GameArea[];
+  sessions?: GameSession[];
+
+}
+
+export interface GameSession {
+  idSession: number;
+
+  sessionNumber: number;
+  sessionTitle: string;
+  when: Date;
+  length: number;
+
+  hands?: Hand[];
+}
+
+export interface GameArea {
+  idArea: number;
+
+  idGame: number; // panrent
+
+  areaTitle: string;
+
+  sCreate: Date;
+  sUpdate: Date;
+  sIdUserCreate?: number;
+  sIdUserUpdate?: number;
+
+  hands?: Hand[];
+}
+
+export type HandState = 'Open' | 'Condensed' | 'Closed' | 'Hidden';
+
+export interface Hand {
+  idHand: number;
+
+  idArea: number; // parent
+
+  handType: string;
+  handTitle: string;
+
+  handState: HandState;
+
+  groupAddable: boolean ;
+
+  cards?: Card[];
+}
+
+export interface Card {
+  idCard: number;
+
+  idHand: number;
+
+  cardTitle: string;
+  description?: string;
+
+  faceImage?: string;
+  backImage?: string;
+
+  headerText?: string;
+  footerText?: string;
+
+  cardType?: string;
+
+  // rollable
+  dieRoll?: string; // e.g. '2d6+1'
+
+  poolValue?: number;
+  currentValue?: number;
+
+
+  customProperties?: Map<string, number>;
 }
--- src/app/root/game.service.ts
+++ src/app/root/game.service.ts
@@ -1,13 +1,14 @@
-import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {Observable, of} from 'rxjs';
 
-import { Game } from '../core/data/game';
+import {Game, GameSession, GameArea} from '../core/data/game';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GameService {
-  constructor() {}
+  constructor() {
+  }
 
   getUserGames$(idUser: number): Observable<Game[]> {
     const fakeGames = [
@@ -51,19 +52,92 @@ export class GameService {
   }
 
   getGame$(idGame: number): Observable<Game> {
+    const gameAreas = [
+      {
+        idArea: 222,
+        idGame: 8323,
+        areaTitle: 'Game',
+        sCreate: new Date('2021-06-07'),
+        sUpdate: new Date('2021-06-07')
+
+      }, {
+        idArea: 223,
+        idGame: 8323,
+        areaTitle: 'Play',
+        sCreate: new Date('2021-06-07'),
+        sUpdate: new Date('2021-06-07')
+      }
+    ] as GameArea[];
+
+    const gameSessions = [{
+      idSession: 138343,
+      sessionNumber: 12,
+      sessionTitle: '12: Gig is Up',
+      when: new Date('2021-06-01'),
+      length: 3,
+      hands: [
+        {
+          idHand: 11,
+          idArea: 138343,
+          handType: '?',
+          handTitle: 'Support',
+          cards: [
+            {
+              idCard: 1111,
+              idHand: 11,
+              cardTitle: 'Ally: Shiori',
+              cardType: 'simple'
+            }
+
+          ]
+        }
+      ]
+    }, {
+      idSession: 138343,
+      sessionNumber: 11,
+      sessionTitle: '11: Raggedy',
+      when: new Date('2021-05-24'),
+      length: 3.5,
+      hands: [
+        {
+          idHand: 11,
+          idArea: 138343,
+          handType: '?',
+          handTitle: 'Reference',
+          cards: [
+            {
+              idCard: 1111,
+              idHand: 11,
+              cardTitle: 'Paraclipse: Shiori',
+              cardType: 'simple'
+            }
+
+          ]
+        }
+      ]
+    }] as GameSession[];
+
     const fake = {
       idGame: 8323,
       gameTitle: 'A Time for Masks',
       imageUrl: 'http://rpg.simplecommunion.com/tfm/itunes.jpg',
-      hoursPlayed: 39,
+      hoursPlayed: 42,
       isActive: true,
       nextGame: new Date('2021-06-01'),
       sCreate: new Date('2021-01-28'),
       sUpdate: new Date('2021-04-19'),
+
       idSystem: 23,
       systemName: 'Cypher*',
 
-    };
+
+      currentSessionId: 138343,
+      areas: gameAreas,
+      sessions: gameSessions,
+      hands: [
+        {}
+      ]
+    } as Game;
 
     return of(fake);
   }
