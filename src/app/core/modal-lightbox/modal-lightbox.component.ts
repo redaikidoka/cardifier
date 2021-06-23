12966e97d0e748c2b011e1e33faf75d757e69828
Pol Stafford <gandalf@Mithrandir.local>
2021-06-23 10:55:32 -0700

    lightbox closes on escape or clickout

--- src/app/core/modal-lightbox/modal-lightbox.component.ts
+++ src/app/core/modal-lightbox/modal-lightbox.component.ts
@@ -1,5 +1,6 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {UnsubscribeOnDestroyAdapter} from '../../root/unsubscribe-on-destroy-adapter';
 
 export interface LightBoxData {
   imageUrl: string;
@@ -13,12 +14,27 @@ export interface LightBoxData {
   templateUrl: './modal-lightbox.component.html',
   styleUrls: ['./modal-lightbox.component.scss']
 })
-export class ModalLightboxComponent implements OnInit {
+export class ModalLightboxComponent extends UnsubscribeOnDestroyAdapter implements OnInit {
 
   constructor(public dialogRef: MatDialogRef<ModalLightboxComponent>,
-              @Inject(MAT_DIALOG_DATA) public data: LightBoxData) { }
+              @Inject(MAT_DIALOG_DATA) public data: LightBoxData) {
+    super();
+  }
 
   ngOnInit(): void {
+    this.subs.sink = this.dialogRef.keydownEvents().subscribe(event => {
+      if (event.key === 'Escape') {
+        this.onCancel();
+      }
+    });
+
+    this.subs.sink = this.dialogRef.backdropClick().subscribe(event => {
+      this.onCancel();
+    });
+  }
+
+  onCancel(): void {
+    this.dialogRef.close(this.data);
   }
 
   closeUp(): void {
